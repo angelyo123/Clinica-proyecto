0a35c3196f2e043ff19ffdfc34004c7cb3e29f9a
Jerson <i202314318@cibertec.edu.pe>
2025-10-24 17:54:02 -0500

    Interceptor Feign en auth

--- backend-clinica/auth-service/src/main/java/com/authservice/client/CitaClient.java
+++ backend-clinica/auth-service/src/main/java/com/authservice/client/CitaClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "cita-service", url = "http://localhost:8084")
+@FeignClient(name = "cita-service", url = "http://localhost:8084" )
 public interface CitaClient {
     @GetMapping("/cita/listar")
     List<Object> listarCitas();
